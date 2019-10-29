9e92c9ebc8e5c4cfc4cffec1c1571190ee8ab2e0
高佳翔 <gaojxcs@gmail.com>
2019-10-29 10:18:27 +0000

    add params

--- annovarindex.go
+++ annovarindex.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"github.com/anarkia7115/annovar_index/read"
 )
 
 func main() {
-	fmt.Println("hello, in main")
-	// read.PrintHeader()
-	read.PassBytes()
+
+	taskHelperText := "task choose in rb, rl, os, rs, ph"
+	task := flag.String("task", "", taskHelperText)
+	inputFile := flag.String("in", "./data/hg19_dbnsfp35a_1m.txt", "input file")
+	chunkSize := flag.Int64("n", 1000, "chunk size for byte read")
+	seekTime := flag.Int("sn", 1, "total time for seek")
+	
+	flag.Parse()
+
+	switch *task {
+	case "rb":
+		read.PassBytes(*inputFile, *chunkSize)
+	case "rl":
+		read.PassLines(*inputFile)
+	case "ph":
+		read.PrintHeader(*inputFile)
+	case "os":
+		read.PassSeekInOrder(*inputFile, *seekTime, *chunkSize)
+	case "rs":
+		read.RandSeek(*inputFile, *seekTime, *chunkSize)
+	default:
+		fmt.Printf("%s\nYou give [%s]\n", taskHelperText, *task)
+	}
 }
